64ecbe5f6ddd29319c320b1c37645f49688736cf
Reward Khe <rewardkhe@outlook.com>
2022-10-12 01:26:01 +1100

    counter react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from "react";
+
+
+function App() { 
+  const [count, setCount] = useState(2);
+  return(
+
+    <div className="container">
+           
+          <h2 classname="num1"> {count} </h2>
+          <button onClick={() => setCount(count +1)}> Increment</button>
+          <button onClick={() => setCount(count * count)}> square </button>
+          <button onClick={() => setCount(count * count)}> root </button>
+          <button onClick={() => setCount(0)} disabled={count === 0}> reset</button>
+        </div>
+     );
 }
 
 export default App;
