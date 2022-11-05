ef509959adf36338220b7deb0e9f0d1aa4c6ebf7
Reward Khe <rewardkhe@outlook.com>
2022-11-06 02:02:33 +1100

    root function fixed

--- src/App.js
+++ src/App.js
@@ -1,29 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React, { useState } from "react";
 
 
@@ -36,7 +10,7 @@ function App() {
           <h2 classname="num1"> {count} </h2>
           <button onClick={() => setCount(count +1)}> Increment</button>
           <button onClick={() => setCount(count * count)}> square </button>
-          <button onClick={() => setCount(count * count)}> root </button>
+          <button onClick={() => setCount(count ** 0.5)}> root </button>
           <button onClick={() => setCount(0)} disabled={count === 0}> reset</button>
         </div>
      );
